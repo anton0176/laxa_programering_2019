97735df5a65a73ef14f85ac95d5b20f6b9c23798
anton0176 <42737675+anton0176@users.noreply.github.com>
2019-02-28 13:44:21 +0100

    Update gissa_rat.java

--- gissa_rat.java
+++ gissa_rat.java
@@ -6,64 +6,67 @@ import java.util.Scanner;
 public class gissa_rat {
 
 	public static void main(String[] args) {
-		
-		Scanner input=new Scanner(System.in);
-		System.out.println("Gissa talet mellan 1 till 100");
-		
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("välj ett tal som är max på gissningsspelet");
+
 		int slump = 0;
 		int antal = 0;
 		int val = 0;
 		boolean exit = false;
 		boolean gissat = false;
-		
-		
-		slump = (int) (Math.random()*100)+1;
-		do  {
-			
-			
-			
-			while (!gissat){
-				val=input.nextInt();
-			
-		
-			if (val<slump){
-				
-				System.out.println("Ditt tal " + val + " är mindre än talet X ");
-				antal ++;
-				
+		int gissa = 0;
+		gissa = input.nextInt();
+		slump = (int) (Math.random() * gissa) + 1;
+
+		do {
+			System.out.println("Talen är mellan 1 till " + gissa);
+
+			while (!gissat) {
+				val = input.nextInt();
+
+				if (val < slump) {
+
+					System.out.println("Ditt tal " + val + " är mindre än talet X ");
+					antal++;
+
+				}
+
+				else if (val > slump) {
+
+					System.out.println("Ditt tal " + val + " är större än talet X ");
+					antal++;
+				}
+
+				else if (val == slump) {
+					System.out.println("Du har gissat rätt det tog " + antal + " gånger");
+					antal++;
+					gissat = true;
+
+				}
+
 			}
-			
-			else if (val>slump) {
-				
-				System.out.println("Ditt tal " + val + " är större än talet X ");
-				antal ++;
-			}
-			
-			else if (val==slump) {
-				System.out.println("Du har gissat rätt det tog " + antal +  " gånger" );
-				antal ++;
-				gissat = true;
-				
-			}
-			
-		}
 			System.out.println("vill du spela igen. 1: ja    2: nej");
-			val=input.nextInt();
-			
+			val = input.nextInt();
+
 			if (val == 1) {
 				gissat = false;
-				slump = (int) (Math.random()*100)+1;
-				System.out.println("Gissa talet mellan 1 till 100");
+				gissa = 0;
+				antal = 0;
+				System.out.println("välj ett tal som är max på gissningsspelet");
+				gissa = input.nextInt();
+				slump = (int) (Math.random() * gissa) + 1;
+
 			}
-		
+
 			else if (val == 2) {
 				exit = true;
-				System.out.println("HEj då ");
+				System.out.println("Tack för att du spela");
 			}
-			
-		}
-		while (!exit);
-		
-	
+
+		} while (!exit);
+
+		input.close();
+
 	}
 }
